524497f0c9c5bde419838767cd747849c6ec8687
Gc8808 <526Gmatos@frhsd.com>
2025-05-12 10:41:41 -0400

    first commit

---
+++ rosters.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const grid = document.getElementById("rosterGrid");
+    const render = list => {
+       grid.innerHTML = ""; // Clear previous content
+       list.forEach(p => {
+           const col = document.createElement("div");
+           col.className = "col-6 col-lg-2";
+              col.innerHTML = `
+                <div class="card h-100 shadow-sm ">
+                     <img src="${p.photo}" class="card-img-top" alt="${p.name}">
+                     <div class="card-body text-center">
+                          <h5 class="card-title mb-1">${p.name}</h5>
+                          <p class="card-text">Age: ${p.age}</p>
+                            <p class="card-text">Country: ${p.country}</p>
+                            <p class="badge badge-position badge-pos-$(p.position}"> ${p.position}</p>
+                        </div>
+                    </div>
+                ` 
+              grid.appendChild(col);
+       });
+    };
+    render(players)
+    // Example usage: render([]); // Pass your list here
+});
